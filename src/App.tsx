ac7c4371edc6530494d39f14acbf89493c503af8
Roman Lariono <larionov.r@giper.fm>
2023-02-17 19:24:28 +0300

    Add react router, react lazy (chang)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
-import Counter from "./Components/Counter";
+import React, { Suspense } from 'react';
 import './index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            BLA Bla bla
-         <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О проекте</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>
+            <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+            </Routes>
+            </Suspense>
         </div>
     );
 };
