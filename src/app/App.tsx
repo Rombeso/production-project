a7c43caf41a45b939d59f378ecc2d176a6af1f90
Roman Lariono <larionov.r@giper.fm>
2023-02-19 18:19:49 +0300

    Setup absolut import in webpack and tsconfig. Start implement feature sliced design.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React, {Suspense, useContext, useState} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./Pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./Pages/MainPage/MainPage.async";
-import {Theme, ThemeContext} from "./theme/ThemeContext";
-import {useTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames";
+import {useTheme} from "app/providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames";
+import {AboutPage} from "pages/AboutPage";
+import {MainPage} from "pages/MainPage";
 
 
 const App = () => {
@@ -19,8 +18,8 @@ const {theme, toggleTheme} = useTheme()
             <Link to={'/about'}>О проекте</Link>
             <Suspense fallback={<div>Загрузка...</div>}>
             <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync/>}/>
-                    <Route path={'/'} element={<MainPageAsync/>}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage/>}/>
             </Routes>
             </Suspense>
         </div>
