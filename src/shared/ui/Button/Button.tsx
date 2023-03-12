d16fae57d3c7b433db2fd196f85310e841ff5c31
Roman Larionov <larionovra@gmail.com>
2023-03-12 20:58:51 +0300

    Add in Storybook button's themes

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,6 +4,7 @@ import cls from './Button.module.scss';
 
 export enum ThemeButton {
     CLEAR = 'clear',
+    OUTLINE = 'outline',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -13,7 +14,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
-        className, children, theme = ThemeButton.CLEAR, ...otherProps
+        className, children, theme, ...otherProps
     } = props;
     return (
         <button
